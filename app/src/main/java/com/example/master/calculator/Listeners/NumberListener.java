b7598cf5dc4b0e320fcafd2aa161bb597a026de8
Masterrom <masterROM@outlook.com>
2018-02-09 14:45:24 -0500

    Decimal Issue resolved

--- app/src/main/java/com/example/master/calculator/Listeners/NumberListener.java
+++ app/src/main/java/com/example/master/calculator/Listeners/NumberListener.java
@@ -6,6 +6,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.master.calculator.Main2Activity;
+
 import org.w3c.dom.Text;
 
 import java.util.regex.Matcher;
@@ -30,13 +32,15 @@ public class NumberListener implements View.OnClickListener {
         Pattern dec = Pattern.compile("^([-]*[0-9]*)");
 
         Pattern decf = Pattern.compile("^(([-]*[0-9]+[.]?)[0-9]*)\\s(.)\\s(([-]*[0-9]*[.]?)[0-9]*)$"); //Applying Not notations, works since the text is a
-
-
-
+        Pattern afterdot = Pattern.compile("^[-]*\\d+[.](\\d*)$");
+        //Possible Patterns
+        Pattern p = Pattern.compile("^(-*(\\d)+.{1}\\d*)\\s.{1}\\s(\\d+.{1}\\d*)");
         String current = this.view.getText().toString();
+
 
         Matcher m = dec.matcher(current);
         Matcher  m2 = decf.matcher(current);
+        Matcher m3 = afterdot.matcher(current);
 
         if (m.matches() && m.group(1).toString().length() <= 15){
             current = current + ((Button)view).getText().toString();
@@ -45,8 +49,12 @@ public class NumberListener implements View.OnClickListener {
         }else if (m2.matches() && m2.group(4).toString().length() <= 15){
             current = current + ((Button)view).getText().toString();
             this.view.setText(current);
+        }else if (m3.matches()){
+            if (m3.group(1).length() < 10){
+                current = current + ((Button)view).getText().toString();
+                this.view.setText(current);
+            }
         }
-
 
 
 
